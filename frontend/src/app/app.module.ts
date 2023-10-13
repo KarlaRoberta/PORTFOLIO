3c01d48815b9ce65f27fc992b2b450c387c1c6ed
KarlaR <karlarobertati@gmail.com>
2023-10-12 22:21:32 -0300

    atualizacao do responsivo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon'
 
 import { HeaderComponent } from './components/header/header.component';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { HeaderComponent } from './components/header/header.component';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
